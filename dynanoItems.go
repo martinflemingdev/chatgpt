89c0151aae0ad57f90ca880b86b75f955951c1a2
Martin Fleming <martinfleming@pop-os.localdomain>
2024-01-25 14:55:05 -0500

    adding dynamotypes

---
+++ dynanoItems.go
@@ -0,0 +1,25 @@
+items := []map[string]dynamotypes.AttributeValue{
+    {
+        "StringField": &dynamotypes.AttributeValueMemberS{Value: "StringValue"},
+        "NumberField": &dynamotypes.AttributeValueMemberN{Value: "123"}, // DynamoDB numbers are strings
+        "FloatField": &dynamotypes.AttributeValueMemberN{Value: "123.45"}, // Representing a float as a string
+        "MapField": &dynamotypes.AttributeValueMemberM{
+            Value: map[string]dynamotypes.AttributeValue{
+                "NestedString": &dynamotypes.AttributeValueMemberS{Value: "NestedValue"},
+                "NestedNumber": &dynamotypes.AttributeValueMemberN{Value: "456"},
+            },
+        },
+        "ListField": &dynamotypes.AttributeValueMemberL{
+            Value: []dynamotypes.AttributeValue{
+                &dynamotypes.AttributeValueMemberS{Value: "ListString"},
+                &dynamotypes.AttributeValueMemberN{Value: "789"},
+            },
+        },
+        "StringSetField": &dynamotypes.AttributeValueMemberSS{Value: []string{"str1", "str2"}},
+        "NumberSetField": &dynamotypes.AttributeValueMemberNS{Value: []string{"100", "200"}},
+        "BinaryField": &dynamotypes.AttributeValueMemberB{Value: []byte("BinaryData")},
+        "BoolField": &dynamotypes.AttributeValueMemberBOOL{Value: true},
+        "NullField": &dynamotypes.AttributeValueMemberNULL{Value: true},
+    },
+    // Additional items can be added here
+}
