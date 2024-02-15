040ab82fea7e8939e3fc4bfe098126c6bf271255
Martin Fleming <martinfleming@pop-os.localdomain>
2024-02-15 11:04:31 -0500

    curl

--- bash/curl-with-json.sh
+++ bash/curl-with-json.sh
@@ -1,1 +1,4 @@
 curl -k -X POST -H "Content-Type: application/json" -d '{"key1": "value1", "key2": {"subkey1": "subvalue1", "subkey2": "subvalue2"}}' https://your-website.com/endpoint
+
+# With auth
+curl -k -X POST -H "Content-Type: application/json" -H "Authorization: Bearer YOUR_TOKEN_HERE" -d '{"key1": "value1", "key2": {"subkey1": "subvalue1", "subkey2": "subvalue2"}}' https://your-website.com/endpoint
