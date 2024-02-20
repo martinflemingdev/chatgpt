c78ea2f3662106e77523f026c5a7d9e9726461be
Martin Fleming <martinfleming@pop-os.localdomain>
2024-02-20 17:03:22 -0500

    capitalize

--- go/capitalize-arn.go
+++ go/capitalize-arn.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 	"regexp"
-	"strings"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 // CapitalizeBetweenColons takes a string and capitalizes the substring between the 3rd and 4th colon
@@ -15,11 +16,12 @@ func CapitalizeBetweenColons(input string) (string, error) {
 		return "", fmt.Errorf("input string does not match the expected format")
 	}
 
-	return matches[1] + strings.Title(matches[2]) + matches[3], nil
+	caser := cases.Title(language.English)
+	return matches[1] + caser.String(matches[2]) + matches[3], nil
 }
 
 func main() {
-	p := "arn:aws:region:non:cloud:cdc:comp:4:s3:"
+	p := "arn:aws:syd:non:cloud:cdc:comp:4:s3:"
 	modified, err := CapitalizeBetweenColons(p)
 	if err != nil {
 		fmt.Println("Error:", err)
