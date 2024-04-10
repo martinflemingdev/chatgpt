4f447ecd32c92a4d532b588ff1c617faf314a92c
Martin Fleming <martinfleming@pop-os.localdomain>
2024-04-10 12:56:31 -0400

    invokeLambda

--- aws-cli/invoke-lambda.sh
+++ aws-cli/invoke-lambda.sh
@@ -13,3 +13,9 @@ aws lambda invoke \
 --invocation-type Event \
 --payload $PAYLOAD \
 outputfile.txt
+
+aws lambda invoke \
+--function-name YourLambdaFunctionName \
+--invocation-type Event \
+--payload '{"Records":[{"messageId":"example-message-id","receiptHandle":"example-receipt-handle","body":"Hello from SQS!","attributes":{"ApproximateReceiveCount":"1","SentTimestamp":"1523232000000","SenderId":"123456789012","ApproximateFirstReceiveTimestamp":"1523232000001"},"messageAttributes":{},"md5OfBody":"example-md5-of-body","eventSource":"aws:sqs","eventSourceARN":"arn:aws:sqs:us-east-1:123456789012:MyQueue","awsRegion":"us-east-1"}]}' \
+outputfile.txt
